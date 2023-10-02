eb6f99c49d236d08739d1ac533c974673d6399c9
mohmmad Smadi <mohmmadmms20@gmail.com>
2023-10-02 16:49:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName='bob';
+function upperCaseCustomerName(){
+     
+     return customerName='BOB'
+}
+function setBestCustomer(){
+    bestCustomer ='not bob'
+}
+function overwriteBestCustomer(){
+    
+    return bestCustomer='maybe bob';
+}
+const leastFavoriteCustomer ="Hello"
+function changeLeastFavoriteCustomer(){
+     return leastFavoriteCustomer="eee"
+}
